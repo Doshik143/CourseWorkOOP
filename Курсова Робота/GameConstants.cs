f809c9ac1c83f04183ed7db0f782a800499b2c33
ipz234_kbb1 <ipz234_kbb1@student.ztu.edu.ua>
2025-02-05 14:30:05 +0200

    Виправлено великі методи та класи

---
+++ Курсова Робота/GameConstants.cs
@@ -0,0 +1,10 @@
+namespace Курсова_Робота
+{
+    public static class GameConstants
+    {
+        public const int RoadResetPosition = 650;
+        public const int CoinRespawnHeight = -50;
+        public const int PlayerMinX = 150;    
+        public const int PlayerMaxX = 700;      
+    }
+}
